6bfff8ecdcaf72e43983382d99eaf1fde5b6eae3
jacksoncougar <jacksoncougar@gmail.com>
2020-01-12 11:18:22 -0700

    Added simple rotation

---
+++ Engine/Engine/Engine/Components/Dirty.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 11/1/20.
+//
+
+#ifndef ENGINE_DIRTY_H
+#define ENGINE_DIRTY_H
+
+
+class Dirty {
+
+};
+
+
+#endif //ENGINE_DIRTY_H
---
+++ Engine/Engine/Engine/Components/Index.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 11/1/20.
+//
+
+#ifndef ENGINE_INDEX_H
+#define ENGINE_INDEX_H
+
+
+class Index {
+
+};
+
+
+#endif //ENGINE_INDEX_H
