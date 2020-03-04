4613d1f520bcb38c51f26ada2dd5ebf5f6d0a347
jacksoncougar <jacksoncougar@gmail.com>
2020-03-04 08:54:10 -0700

    wip

--- Engine/Components/EventHandler.h
+++ Engine/Components/EventHandler.h
@@ -27,14 +27,16 @@ namespace Component {
 	public:
 		std::function<void(const Component::EventArgs<Args...>&)> callback;
 		bool invoke_all() override {
+
 			bool invoked_callback = false;
 			auto args = getChildren().getComponentsOfType<EventArgs<Args...>>();
 			for (auto& arg : args) {
 				callback(*arg);
 				invoked_callback = true;
 			}
-			return invoked_callback;
 			getChildren().eraseComponentsOfType<Component::EventArgs<Args...>>();
+
+			return invoked_callback;
 		}
 	};
 
