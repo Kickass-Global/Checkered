0a8cca68b3d43595b7fac6e823a0e1c670668ad4
JacksonCougar <jacksoncougar@gmail.com>
2020-03-05 15:54:32 -0700

    - wip

--- Engine/Components/scenery.hpp
+++ Engine/Components/scenery.hpp
@@ -10,6 +10,17 @@
 
 namespace Component {
 
+	template <typename T>
+	class Instance : public ComponentBase {
+	public:
+		std::vector<std::shared_ptr<T>> instance_node;
+
+		template <typename... Args>
+		void add_instance_at(glm::vec3 position, Args... args) {
+			instance_node.push_back(Engine::createComponent<T>(position, args...));
+		}
+	};
+
 	/**
 	 * Scenery components are things that can be placed into the world and have collision properties
 	 */
@@ -19,10 +30,11 @@ namespace Component {
 		std::shared_ptr<PhysicsActor> actor;
 		std::shared_ptr<PaintedMesh> mesh;
 
-		Scenery(std::shared_ptr<Mesh> mesh, std::shared_ptr<Material> material) :
+		Scenery(glm::vec3 position, std::shared_ptr<Mesh> mesh, std::shared_ptr<Material> material) :
 			mesh(Engine::createComponent<PaintedMesh>(mesh, material)),
 			actor(Engine::createComponent<PhysicsActor>(mesh))
 		{
+			actor->position = position;
 			actor->node->addChildComponent(Engine::createComponent<SceneComponent>(actor->node, this->mesh));
 		}
 	};
