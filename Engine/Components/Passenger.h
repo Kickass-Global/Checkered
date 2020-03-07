842edc32e1319488c3de398413e242d22f220ffa
JacksonCougar <jacksoncougar@gmail.com>
2020-03-07 14:51:17 -0700

    -WIP

--- Engine/Components/Passenger.h
+++ Engine/Components/Passenger.h
@@ -19,9 +19,11 @@ namespace Component {
 	class Passenger : public ComponentBase {
 
 	public:
-		
+
 		std::shared_ptr<Waypoint> pickup_actor;
 		std::shared_ptr<Waypoint> dropoff_actor;
+		EventDelegate<int> onPassengerPickedUpDelegate{""};
+		EventDelegate<int> onPassengerDroppedUpDelegate{""};
 
 		PxPhysics* pass_physics;
 		PxMaterial* pass_material;
@@ -32,7 +34,10 @@ namespace Component {
 		void setPickupTransform(PxTransform pickupTrans);
 		void setDropoffTransform(PxTransform dropoffTrans);
 
-		Passenger() {
+		Passenger(glm::vec3 pickup_location, glm::vec3 dropoff_location, std::shared_ptr<Mesh> mesh, std::shared_ptr<Material> material) {
+
+			pickup_actor = Engine::createComponent<Waypoint>(glm::vec3{ 0.f, 0.f, 2.0f }, mesh, material);
+			dropoff_actor = Engine::createComponent<Waypoint>(glm::vec3{ 13.0f, 0.f, 2.0f }, mesh, material);
 			//pickup_actor->onEntered += std::bind(&Passenger::onPassengerPickedUp, this, std::placeholders::_1);
 		}
 
@@ -49,7 +54,7 @@ namespace Component {
 	};
 
 
-	
+
 
 
 }
