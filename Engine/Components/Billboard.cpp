9b28d8a3cef516520b91c4a3f74e26267ab1915a
jacksoncougar <jacksoncougar@gmail.com>
2020-03-28 15:11:39 -0600

    - added arrow that points to the pickup(?) location

--- Engine/Components/Billboard.cpp
+++ Engine/Components/Billboard.cpp
@@ -4,17 +4,21 @@
 
 #include "Billboard.h"
 #include <Mesh.h>
+#include "Component.h"
+#include "texture.hpp"
+#include "material.hpp"
 #include <Pipeline/Library.h>
 
-Billboard::Billboard(std::shared_ptr<Texture> sprite) {
-    auto material = getEngine()->createComponent<Material>(
+Billboard::Billboard(const std::shared_ptr<Texture> &billboard_image) {
+    auto material = getEngine()->createComponent<Component::Material>(
         getEngine()->getSubSystem<Pipeline::Library>()->getAsset<Program>(
             "Assets/Programs/billboard.json"
         ));
-    material->textures.push_back(sprite);
-    mesh_instance = getEngine()->createComponent<PaintedMesh>(
-        getEngine()->getSubSystem<Pipeline::Library>()->getAsset<Mesh>(
-            "Assets/Meshes/billboard_quad.obj"
-        ), material
+    material->textures.push_back(billboard_image);
+    auto billboard_mesh = getEngine()->getSubSystem<Pipeline::Library>()->getAsset<Mesh>(
+        "Assets/Meshes/billboard_quad.obj"
+    );
+    mesh_instance = getEngine()->createNamedComponent<PaintedMesh>(
+        "billboard_mesh_instance", billboard_mesh, material
     );
 }
